863e621bd992d7cb87e375308f14a3a7031b4961
alexmedkex <alexmedkex@gmail.com>
2017-11-12 13:14:11 +0100

    Added styling to header. Added icon for file upload. Added bootstrap styling.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
 import logo from './fa-icon-ethereum.svg';
+import uploadimg from './upload.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Deploy your smart contract to the blockchain!</h1>
-          </header>
-          <p className="App-intro">
-            Source code:
-            <input id="input" type="file" accept=".sol"/>
-              Contract name:
-            <input id="name" type="text"/>
-          </p>
-        </div>
-        );
-  }
+    render() {
+        return (
+                <div className="App">
+                    <header className="App-header">
+                        <h1 id="title"><span>Deploy.eth</span></h1>
+                        <img src={logo} className="App-logo" alt="logo" />
+                    </header>
+                    <div id="mainContent">
+                        <div id="upload-div" className="row">
+                            <div className="col-sm-12" id="file-upload">
+                                <label for="input">
+                                    <img src={uploadimg}/>
+                                </label>
+                                <input id="input" type="file" accept=".sol"/>
+                            </div>
+
+                        </div>
+                        <div id="contract-name" className="row">
+                            <div className="col-sm-12">
+                                Contract name:
+                                <input id="name" type="text"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                );
+    }
 }
 
 export default App;
